e3acfbb908830ec7afd0bc86825c4f58c3d94238
lufemas <maschietto.jr@gmail.com>
2023-06-30 23:13:29 -0300

    making the url, use tupples

--- src/components/PeriodInput.tsx
+++ src/components/PeriodInput.tsx
@@ -10,14 +10,17 @@ const formatDate = (dateToFormat: any) => {
 };
 
 // @ts-ignore 
-export default function PeriodInput({ id = '', onChange, onRemove }) {
+export default function PeriodInput({ id = '', onChange, onRemove, inputValue='', loadArriveDate='', loadDepartureDate='' }) {
+  // console.log('Single, inputValue', inputValue)
   const [arriveDate, setArriveDate] = useState("");
   const [departureDate, setDepartureDate] = useState("");
   const [daysInLand, setDaysInLand] = useState(0);
+  const [fullDaysInLand, setFullDaysInLand] = useState(0);
+  const [isPR, setIsPR] = useState(true);
 
   const handleDateChange = (dateChanged: string) => {
     setArriveDate(dateChanged);
-    console.log("dateChanged", dateChanged);
+    // console.log("dateChanged", dateChanged);
   };
 
   const calculateDaysInLand = (initialDate: Date, finalDate: Date) => {
@@ -27,34 +30,59 @@ export default function PeriodInput({ id = '', onChange, onRemove }) {
     const timeDiff = date2.getTime() - date1.getTime();
     const daysDiff = Math.floor(timeDiff / (24 * 60 * 60 * 1000));
 
-    console.log("daysDiff +1", daysDiff + 1); // Output: 7
+    // console.log("daysDiff +1", daysDiff + 1); // Output: 7
 
-    return daysDiff + 1;
+    return (daysDiff + 1) || 0;
   };
 
   useEffect(() => {
-    setDaysInLand(
-      calculateDaysInLand(new Date(arriveDate), new Date(departureDate))
-    );
+    const totalDaysInLand = calculateDaysInLand(new Date(arriveDate), new Date(departureDate));
+    setDaysInLand(totalDaysInLand);
 
-    onChange({ id, arriveDate, departureDate, daysInLand });
-  }, [arriveDate, departureDate]);
+    setFullDaysInLand( isPR ? totalDaysInLand : totalDaysInLand / 2);
+
+    onChange({ id, arriveDate, departureDate, daysInLand, fullDaysInLand, isPR });
+  }, [arriveDate, departureDate, isPR]);
 
   useEffect(() => {
-    console.log("daysInLand", daysInLand); // Output: 7
-    onChange({ id, arriveDate, departureDate, daysInLand });
+    // console.log("daysInLand", daysInLand); // Output: 7
+    onChange({ id, arriveDate, departureDate, daysInLand, fullDaysInLand, isPR });
   }, [daysInLand]);
+
+  useEffect(()=>{
+    // if(inputValue) {
+    //   console.log('got inputValue', inputValue)
+    //   setArriveDate(inputValue["arriveDate"]);
+    //   setDepartureDate(inputValue["departureDate"]);
+    // }
+    console.log("loadArriveDate", loadArriveDate)
+
+    if(loadArriveDate != '') {
+      console.log("with loadArriveDate", loadArriveDate)
+      setArriveDate(loadArriveDate)
+      console.log("arriveDate", arriveDate)
+    }
+
+    if(loadDepartureDate != '') {
+      console.log("with loadDepartureDate", loadDepartureDate)
+      setDepartureDate(loadDepartureDate)
+      console.log("DepartureDate", departureDate)
+    }
+  },[])
   return (
     <div className="period-input">
       <h3>{id || "-"}</h3>
+      <label htmlFor="pr-checkbox"> PR
+        <input type="checkbox" name="pr-checkbox" id="pr-checkbox" checked={isPR} onChange={()=>setIsPR(wasPR => !wasPR)}/>
+      </label>
       <label htmlFor="dateIn">
         Arrive Date
         <input
           type="date"
           name="dateIn"
           id="dateIn"
-          // value={arriveDate}
-          onChange={(e) => setArriveDate(e.target.value)}
+          value={arriveDate}
+          onChange={(e) => {console.log("e.target.value", e.target.value ); setArriveDate(e.target.value)}}
         />
       </label>
       <label htmlFor="dateIn">
@@ -63,13 +91,17 @@ export default function PeriodInput({ id = '', onChange, onRemove }) {
           type="date"
           name="dateIn"
           id="dateIn"
+          value={departureDate}
           onChange={(e) => setDepartureDate(e.target.value)}
         />
       </label>
       <span>
         {daysInLand} Day{daysInLand > 1 && "s"}
       </span>
-      <button onClick={onRemove ? onRemove : () => {}}>
+      <span>
+        {fullDaysInLand} Day{daysInLand > 1 && "s"}
+      </span>
+      <button onClick={onRemove ? ()=>onRemove(id) : () => {}}>
         <span role="img">🗑️</span>
       </button>
     </div>
--- src/components/TotalCalculator.tsx
+++ src/components/TotalCalculator.tsx
@@ -1,62 +1,131 @@
 import { useEffect, useState } from "react";
 import PeriodInput from "../components/PeriodInput";
 import { v4 as uuidv4 } from "uuid";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
+  const [grossTotalDays, setGrossTotalDays] = useState(0);
   const [periodInputs, setPeriodInputs] = useState([] as JSX.Element[]);
-  const [inputValues, setInputValues] = useState({} as any);
+  const [inputValues, setInputValues]
+    = useState({
+      [uuidv4()]: {
+            // arriveDate: "",
+            // daysInLand: 0,
+            // departureDate: "",
+            // id: uuidv4()
+          }} as any);
 
   const onRemoveInput = (index: any) => {
-    console.log("removing:", index);
+    // console.log("removing:", index);
+    // setPeriodInputs(periodInputs.filter(periodInput => periodInput.props.id != index));
+    setInputValues( prevInputValues => {
+      const newInputvalues = {...prevInputValues};
+      delete newInputvalues[index];
+      return newInputvalues;
+
+    });
+
   };
 
-  const onInputChage = (input: any) => {
-    const id = input.id;
-    console.log("chaging:", input);
-    setInputValues({ ...inputValues, [`${id}`]: { ...input } });
+  const onInputChange = (changeInput: any) => {
+    const id = changeInput.id;
+    console.log("chaging:", changeInput);
+
+    Object.values(inputValues).forEach( (input: any) => {
+      if(!input.id != changeInput.id ) {
+        const intersect = areDatePeriodsIntersecting(input.arriveDate, input.departureDate, changeInput.arriveDate, changeInput.departureDate);
+        console.log('intersect', intersect)
+        console.log(input)
+        if(intersect) {
+          return toast.warn("Overlapping date", {autoClose: 1000,})
+        }
+      }
+    })
+
+    setInputValues({ ...inputValues, [`${id}`]: { ...changeInput } });
   };
 
   const addPeriodInput = () => {
-    const newElement = (
-      <PeriodInput
-        id={uuidv4()}
-        onChange={onInputChage}
-        onRemove={onRemoveInput}
-      />
-    );
-    setPeriodInputs([...periodInputs, newElement]);
+    // const newElement = (
+    //   <PeriodInput
+    //     id={uuidv4()}
+    //     onChange={onInputChange}
+    //     onRemove={onRemoveInput}
+    //     inputValue={'a'}
+    //   />
+    // );
+    // setPeriodInputs([...periodInputs, newElement]);
+    setInputValues({...inputValues, [uuidv4()]: {}})
   };
+
+  const getUrl = () => {
+
+    // let dates = [];
+    // for (const [key, value] of Object.entries(inputValues)) {
+    //   dates
+    // }
+
+    const jsonString = JSON.stringify(inputValues);
+    const encodedJsonString = encodeURIComponent(jsonString);
+    return window.location.href + "?data=" + encodedJsonString;
+  }
+  const areDatePeriodsIntersecting = (start1, end1, start2, end2) => {
+    return start1 <= end2 && start2 <= end1;
+  }
 
   useEffect(() => {
     console.clear();
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const encodedJsonString = urlParams.get('data');
+    const jsonString = decodeURIComponent(encodedJsonString);
+    const data = JSON.parse(jsonString);
+    console.log( 'jsonString', jsonString)
+    console.log( 'parserd', data)
+
+    if(data){
+      let loadedInputValues = {};
+      for (const [key, value] of Object.entries(data)) {
+        console.log('key', key)
+        console.log('value', value)
+        loadedInputValues = {...loadedInputValues, [key]: {...value as object}}
+      }
+      setInputValues(loadedInputValues)
+      console.log('loadedInputValues', loadedInputValues)
+
+    }
+
   }, []);
   useEffect(() => {
     // addPeriodInput();
-    console.log("useEffect inputValues", inputValues);
+    // console.log("useEffect inputValues", inputValues);
 
     let isAllPeriodsFilles = true;
 
     Object.values(inputValues).forEach( (input: any) => {
       if(!input.daysInLand) {
-        console.log(input)
+        // console.log(input)
       isAllPeriodsFilles = false;
       }
     })
 
     if(isAllPeriodsFilles) {
       addPeriodInput();
-      console.log('addinf new input')
+      // console.log('addinf new input')
 
     }
 
-    // const checkForOpenDate = Object.keys(inputValues).filter((input) => {
-    //   console.log("input", inputValues[input]);
-    //   return inputValues[input].daysInLand;
-    // });
+    // Set Gross Total Days
+    let grossTotalDaysTemp: number = 0;
+    for (const [key, value] of Object.entries(inputValues)) {
+      // console.log(`${key}: ${value}`);
+      grossTotalDaysTemp += value["daysInLand"] || 0;
+    }
+    // console.log('grossTotalDaysTemp', grossTotalDaysTemp)
+    setGrossTotalDays(grossTotalDaysTemp)
 
-    // if (checkForOpenDate) addPeriodInput();
-    // if (checkForOpenDate) console.log('GOOOOO');
-    // console.log("checkForOpenDate", checkForOpenDate);
+    console.log(getUrl())
   }, [inputValues]);
 
   useEffect(() => {
@@ -64,16 +133,36 @@ export default function App() {
   }, [inputValues]);
 
   return (
-    <div className="total-calculator">
+    <>
+    {/* <div className="total-calculator"> */}
       <h1>Calculator 0</h1>
 
-      {/* <PeriodInput id={1} onChange={onInputChage} onRemove={onRemoveInput} /> */}
+      {/* <PeriodInput id={1} onChange={onInputChange} onRemove={onRemoveInput} /> */}
 
-      {periodInputs.map((periodInput, index) => {
+      {/* {periodInputs.map((periodInput, index) => {
         return periodInput;
-      })}
+      })} */}
+
+      { Object.keys(inputValues).map((inputValueKey) => {
+        // console.log('mapping key', inputValueKey)
+        return <PeriodInput 
+                key={inputValueKey}
+                id={inputValueKey}
+                onChange={onInputChange}
+                onRemove={onRemoveInput}
+                loadArriveDate={inputValues[inputValueKey]["arriveDate"]}
+                loadDepartureDate={inputValues[inputValueKey]["departureDate"]}
+                inputValue={inputValues[inputValueKey]}/>
+        
+        })
+      }
       {/* <PeriodInput /> */}
-      <p>Total Days in Land: </p>
-    </div>
+
+      <button onClick={addPeriodInput}>➕</button>
+      <p>Total Days in Land: {grossTotalDays}</p>
+      <a href={getUrl()} target="_blank" rel="noopener noreferrer">{getUrl()}</a>
+    {/* </div> */}
+    <ToastContainer></ToastContainer>
+      </>
   );
 }
